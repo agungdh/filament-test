6ef3572123caad9b006c58f91820e237df856e07
AgungDH <agungdh@live.com>
2025-01-18 17:54:38 +0700

    WIP: docker run

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+docker run -d \
+-p 80:80 \
+-v ${PWD}/.env:/var/www/html/.env \
+-v app_logs:/var/www/html/storage/logs \
+filament
+
 #docker run -d -p 80:80 -v ${PWD}/logs:/var/www/html/storage/logs filament
-docker run -d -p 80:80 filament
+#docker run -d -p 80:80 -v app_logs:/var/www/html/storage/logs filament
+#docker run -d -p 80:80 filament
