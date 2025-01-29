44000a13b1f8e0332498354e6808669f5e356976
AgungDH <agungdh@live.com>
2025-01-29 13:26:16 +0700

    cron test

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\CronTest;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    $dummy = new CronTest();
+
+    $dummy->isi = Inspiring::quote();
+
+    $dummy->save();
+})->everySecond();
